af0ad9b083029963e6aa8ee46eb7a8b9ddb47edf
csalguera <carlos.e.salguera@gmail.com>
2025-09-15 00:16:39 -0400

    add banners to home page through DOM for scalability

--- homework-1/js/app.js
+++ homework-1/js/app.js
@@ -1,16 +1,22 @@
+// cached element references
 const navRef = document.querySelector('.navbar')
+const bannersRef = document.querySelector('.banners-container')
 
+// home link
 const home = document.createElement('a')
 home.href = 'index.html'
 home.textContent = 'Home'
 
+// dropdown
 const dropDown = document.createElement('div')
 dropDown.className = 'dropdown'
 
+// titles button
 const dropBtn = document.createElement('button')
 dropBtn.className = 'dropbtn'
 dropBtn.textContent = 'Titles'
 
+// title links
 const dropDownContent = document.createElement('div')
 dropDownContent.className = 'dropdown-content'
 
@@ -30,8 +36,27 @@ titles.forEach(({ href, text }) => {
 
 dropDown.append(dropBtn, dropDownContent)
 
+// rules link
 const rules = document.createElement('a')
 rules.href = 'rules.html'
 rules.textContent = 'Rules'
 
 navRef.append(home, dropDown, rules)
+
+const banners = [
+  { href: 'blc.html', src: 'images/blc-banner.png', alt: 'bleach banner' },
+  { href: 'htr.html', src: 'images/htr-banner.png', alt: 'hunter x hunter banner' },
+  { href: 'jjk.html', src: 'images/jjk-banner.png', alt: 'jujutsu kaisen banner' },
+  { href: 'cgh.html', src: 'images/cgh-banner.png', alt: 'code geass banner' },
+]
+
+banners.forEach(({ href, src, alt }) => {
+  const a = document.createElement('a')
+  a.href = href
+  const img = document.createElement('img')
+  img.src = src
+  img.alt = alt
+  img.className = 'banner'
+  a.appendChild(img)
+  bannersRef.appendChild(a)
+})
