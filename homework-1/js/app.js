fc0abca9e3eeed51a57cc66353cc56b27c570efb
csalguera <carlos.e.salguera@gmail.com>
2025-09-14 23:59:26 -0400

    add js code to render navbar via DOM manipulation

---
+++ homework-1/js/app.js
@@ -0,0 +1,37 @@
+const navRef = document.querySelector('.navbar')
+
+const home = document.createElement('a')
+home.href = 'index.html'
+home.textContent = 'Home'
+
+const dropDown = document.createElement('div')
+dropDown.className = 'dropdown'
+
+const dropBtn = document.createElement('button')
+dropBtn.className = 'dropbtn'
+dropBtn.textContent = 'Titles'
+
+const dropDownContent = document.createElement('div')
+dropDownContent.className = 'dropdown-content'
+
+const titles = [
+  { href: 'titles/blc.html', text: 'Bleach' },
+  { href: 'titles/htr.html', text: 'Hunter x Hunter' },
+  { href: 'titles/jjk.html', text: 'Jujutsu Kaisen' },
+  { href: 'titles/cgh.html', text: 'Code Geass' },
+]
+
+titles.forEach(({ href, text }) => {
+  const a = document.createElement('a')
+  a.href = href
+  a.textContent = text
+  dropDownContent.appendChild(a)
+})
+
+dropDown.append(dropBtn, dropDownContent)
+
+const rules = document.createElement('a')
+rules.href = 'rules.html'
+rules.textContent = 'Rules'
+
+navRef.append(home, dropDown, rules)
